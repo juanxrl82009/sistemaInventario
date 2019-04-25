a5df6ef0e931ac91e27bdf5c753457e36c322126
danie <danie@DESKTOP-A8AIRVE>
2019-04-25 13:12:46 -0500

    Corregido Usuario

---
+++ src/Modelo/Factura.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author danie
+ */
+
+import java.util.Date;
+
+public class Factura{	
+    private int IdCompra;
+    private int IdFacturacompra;
+    private int IdUsuario;
+    private int NitProveedor;
+    private Date FechaCompra;
+    private int TotalCompra;
+    	
+
+     public int getIdCompra() {	
+        return IdCompra;	
+    }	
+
+     public void setIdCompra(int IdCompra) {	
+        this.IdCompra = IdCompra;	
+    }	
+
+     public int getIdFacturacompra() {	
+        return IdFacturacompra;	
+    }	
+
+     public void setIdFacturacompra(int IdFacturacompra) {	
+        this.IdFacturacompra = IdFacturacompra;	
+    }	
+     public int getIdUsuario() {	
+        return IdUsuario;	
+    }	
+
+     public void setIdUsuario(int IdUsuario) {	
+        this.IdUsuario = IdUsuario;	
+    }	
+     public int getNitProveedor() {	
+        return NitProveedor;	
+    }	
+
+     public void setNitProveedor(int NitProveedor) {	
+        this.NitProveedor = NitProveedor;	
+    }	
+     public Date getFechaCompra() {	
+        return FechaCompra;	
+    }	
+
+     public void setFechaCompra(Date FechaCompra) {	
+        this.FechaCompra = FechaCompra;	
+    }	
+     
+     public int getTotalCompra() {	
+        return TotalCompra;	
+    }	
+
+     public void setTotalCompra(int TotalCompra) {	
+        this.TotalCompra = TotalCompra;	
+    }	
+
+
+ } 
